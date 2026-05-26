862a5c6f24116f4d020ceb06e47b13bb892e5e38
aipv <qingzhou.berlin@gmail.com>
2026-05-26 11:26:23 +0200

    support plane

--- esp32_student/main/main.c
+++ esp32_student/main/main.c
@@ -4,27 +4,14 @@
  * SPDX-License-Identifier: CC0-1.0
  */
 
-#include <stdio.h>
-#include <inttypes.h>
-#include "sdkconfig.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_log.h"
-#include "wifi_station.h"
-#include "mqtt_client_app.h"
-#include "ota_http_client.h"
+#include "support_plane.h"
 
 static const char *TAG = "MAIN_APP";
 
 void app_main(void)
 {
-    ota_confirm_after_start();
-
-    wifi_station_init();
-    ESP_LOGI(TAG, ">>> End of initiation, starting tasks...");
-
-    mqtt_client_start();
-    ESP_LOGI(TAG, ">>> End of starting tasks, system running...");
+    support_plane_init();
+    ESP_LOGI(TAG, "support_plane_init() Done!");
 
     while (1)
     {
