8ff6d13311ab9d313ef9a965355bd38b73727ede
aipv <qingzhou.berlin@gmail.com>
2026-05-28 14:00:36 +0200

    add Counter

--- esp32_student/components/execute_plane/execute_plane.c
+++ esp32_student/components/execute_plane/execute_plane.c
@@ -18,6 +18,51 @@ static char send_buffer[37096];
 static int32_t *pcm_data = (int32_t *)(data_buffer);
 static int16_t *pcm16_data = (int16_t *)(send_buffer);
 
+typedef struct {
+    int32_t    count;
+    float      min;
+    float      max;
+    float      sum;
+    double     sum2;
+} CoefStatistic;
+CoefStatistic   Counter[13];
+
+void counter_init()
+{
+    for (int i = 0; i < 13; i++)
+    {
+        Counter[i].count = 0;
+        Counter[i].min = 0;
+        Counter[i].max = 0;
+        Counter[i].sum = 0;
+        Counter[i].sum2 = 0;
+    }
+}
+
+void counter_update(float *data)
+{
+    for (int i = 0; i < 98; i++)
+    {
+        for (int j = 0; j < 13; j++)
+        {
+            float coef = data[i * 13 + j];
+            Counter[j].min = (coef < Counter[j].min) ? coef : Counter[j].min;
+            Counter[j].max = (coef > Counter[j].max) ? coef : Counter[j].max;
+            Counter[j].sum += coef;
+            Counter[j].sum2 += coef * coef;
+            Counter[j].count++;
+        }
+    }
+}
+
+void counter_print()
+{
+    for (int i = 0; i < 13; i++)
+    {
+        ESP_LOGI(TAG, "%d : %d, %f, %f, %f, %lf", i, Counter[i].count, Counter[i].min, Counter[i].max, Counter[i].sum, Counter[i].sum2);
+    }
+}
+
 esp_err_t pcm16_mfcc_preprocess(int16_t *input_pcm, int count)
 {
     int16_t *iptr = input_pcm;
@@ -52,7 +97,30 @@ void application_button_boot_callback(uint8_t gpio_num)
 void application_button_up_callback(uint8_t gpio_num)
 {
     ESP_LOGW(TAG, ">>> Button Up (GPIO %d) Pressed! - Executing action B.", gpio_num);
-    i2s_audio_stream_data(1);
+    counter_init();
+    counter_print();
+    if (network_socket_init() < 0)
+    {
+        ESP_LOGE(TAG, "Failed to connect to host.");
+        return;
+    }
+    for (int i = 0; i < 600; i++)
+    {
+        i2s_audio_read_data(pcm_data, count);
+        i2s_audio_convert_data(pcm_data, pcm16_data, count);
+        pcm16_mfcc_preprocess(pcm16_data, count);
+        counter_update((float *)(pcm16_data + count));
+        if (((i + 1) % 60) == 0) counter_print();
+        int bytes_sent = network_socket_send(pcm16_data, 37096);
+        if (bytes_sent != 37096)
+        {
+            ESP_LOGE(TAG, "Transmission failed or incomplete!");
+        }
+    }
+
+    network_socket_close();
+    counter_print();
+    ESP_LOGI(TAG, "Success Stream 100 seconds data!");
 }
 
 void application_button_down_callback(uint8_t gpio_num)
